060191cad22a41dbf8ad7986d5fe728c452b12bf
bhossa <bhossa@gmail.com>
2021-03-18 03:04:01 +0000

    Message Class Description Test 1

--- command.js
+++ command.js
@@ -1,8 +1,8 @@
 class Command {
    constructor(commandType, value) {
      this.commandType = commandType;
-     if (!commandType) {
-       throw Error("Command type required.");
+    if (!commandType) {
+      throw Error("Command type required.");
      }
      this.value = value;
    }
--- message.js
+++ message.js
@@ -1,5 +1,12 @@
 class Message {
    // Write code here!
+   constructor(name, commands){
+     this.name = name;
+    if(!name){
+      throw Error("Message name required.");
+    }
+       
+    }
 }
 
 module.exports = Message;
